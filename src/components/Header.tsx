efa74d554409d52e38e8fc3b08de7b6146ca2d3d
2ZS7 <szulfira79@gmail.com>
2026-03-25 01:48:12 +0500

    fix: выравнивание  ссылки с правой стороны

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,6 +8,7 @@ export default function Header() {
         <nav className="p-4 bg-gray-900 text-white flex gap-6 shadow-lg mb-6">
             <Link to="/" className="font-bold hover:text-blue-400">Dashboard</Link>
             <Link to="/rules" className="font-bold hover:text-blue-400">Редактор правил</Link>
+            <Link to="/" className="ml-auto font-bold hover:text-blue-400">Stateful Mock Engine</Link>
         </nav>
     );
 }
